d6e307bdea334c840b0a52d3fc0060c30cee12b2
unknown <pallavigadge3194@gmail.com>
2025-07-05 15:34:16 +0530

    SaceDemo Framework Created

---
+++ src/main/java/baseLayer/BaseClass.java
@@ -0,0 +1,10 @@
+package baseLayer;
+
+public class BaseClass {
+	
+	public void setUp(String driverName) {
+		
+		System.out.println("Enter browser name ::"+driverName);
+	}
+
+}
---
+++ src/test/java/testLayer/BaseTest.java
@@ -0,0 +1,14 @@
+package testLayer;
+
+import baseLayer.BaseClass;
+
+public class BaseTest {
+	
+	public static void main(String[] args) {
+		
+		BaseClass base=new BaseClass();
+		
+		base.setUp("chrome");
+	}
+
+}
