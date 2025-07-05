7925013018d586863f712a2271add5e5e571c2ce
unknown <pallavigadge3194@gmail.com>
2025-07-05 16:56:17 +0530

    Home page fucntionality

--- src/test/java/testLayer/TestClass.java
+++ src/test/java/testLayer/TestClass.java
@@ -1,14 +1,19 @@
 package testLayer;
 
 import baseLayer.BaseClass;
+import pageLayer.HomePage;
 
-public class BaseTest {
+public class TestClass {
 	
 	public static void main(String[] args) {
 		
 		BaseClass base=new BaseClass();
 		
 		base.setUp("chrome");
+		
+		HomePage home=new HomePage();
+		
+		home.validateHomePage();
 	}
 
 }
